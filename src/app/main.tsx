a3400520dd031fe5c7d08348bace26e3a502d579
cdricn <cdricncdricn@gmail.com>
2025-07-27 17:23:30 +0800

    Updated design

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,14 +1,29 @@
+import styles from './main.module.css'
 import Calendar from './calendar/calendar'
 import Schedule from './schedule/schedule'
 
 export default function Main() {
 
   return (
-    <main className='content-container'>
-      <div className='calendar-container'>
+    <main className={styles['content-container']}>
+      <div className={styles['calendar-container']}>
+        <ul className={styles['calendar-months']}>
+          <li>APR</li>
+          <li>MAY</li>
+          <li>JUN</li>
+          <li>JUL</li>
+          <li>AUG</li>
+          <li>SEP</li>
+          <li>OCT</li>
+          <li>NOV</li>
+          <li>DEC</li>
+          <li>JAN</li>
+          <li>FEB</li>
+          <li>MAR</li>
+        </ul>
         <Calendar />
       </div>
-      <div className='schedule-container'>
+      <div className={styles['schedule-container']}>
         <Schedule />
       </div>
     </main>
