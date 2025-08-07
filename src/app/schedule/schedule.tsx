7ecb075c36befeeec89041575e97768c275b889c
cdricn <cdricncdricn@gmail.com>
2025-08-07 22:21:19 +0800

    Added schedules section

--- src/app/schedule/schedule.tsx
+++ src/app/schedule/schedule.tsx
@@ -1,8 +1,23 @@
+import styles from './schedule.module.css'
 
 export default function Schedule() {
 
   return (
-    <>
-    </>
+    <div className={styles['schedule']}>
+      <div className={styles['schedule-nav']}>
+        <div className={styles['schedule-date']}>
+          <p>8/10 Wed</p>
+        </div>
+        <div className={styles['schedule-filters']}>
+          <div>Events</div>
+          <div>Confidants</div>
+          <div>Jobs</div>
+          <div>Day/Night</div>
+        </div>
+      </div>
+      <div className={styles['schedule-info']}>
+        
+      </div>
+    </div>
   )
 }
