df0ecf28cee7e6a007a6352cffa165b9e6159ecc
cdricn <cdricncdricn@gmail.com>
2025-08-07 22:22:22 +0800

    Updated main component code

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,30 +1,62 @@
 import styles from './main.module.css'
 import Calendar from './calendar/calendar'
 import Schedule from './schedule/schedule'
+import { fetchCalendarData } from './lib/data'
+import { testData } from './calendar/testData'
+import { useState } from 'react'
 
 export default function Main() {
+  // import test data depending on month selected. Default is data from April, the 1st month
+  // Pass digits to Calendar component to form its structure
+  // Pass object data to schedule to display stuff based on selected number 
+  const [data, setData] = useState([])
+  const [month, setMonth] = useState('APR')
+
+  function populateMonths() {
+    const months = [
+      'APR',
+      'MAY',
+      'JUN',
+      'JUL',
+      'AUG',
+      'SEP',
+      'OCT',
+      'NOV',
+      'DEC',
+      'JAN',
+      'FEB',
+      'MAR'
+    ]
+    return months.map((i, index)=>{
+      return (
+        <li key={index} onClick={()=>setSelectedMonth(i)}>{i}</li>
+      )
+    })
+  }
+  
+  function setSelectedMonth(e:string) {
+    setMonth(e)
+    console.log(month)
+  }
+
+  function handleTest(e:any) {
+
+  }
 
   return (
-    <main className={styles['content-container']}>
-      <div className={styles['calendar-container']}>
-        <ul className={styles['calendar-months']}>
-          <li>APR</li>
-          <li>MAY</li>
-          <li>JUN</li>
-          <li>JUL</li>
-          <li>AUG</li>
-          <li>SEP</li>
-          <li>OCT</li>
-          <li>NOV</li>
-          <li>DEC</li>
-          <li>JAN</li>
-          <li>FEB</li>
-          <li>MAR</li>
-        </ul>
-        <Calendar />
-      </div>
-      <div className={styles['schedule-container']}>
-        <Schedule />
+    <main className={styles['main-container']}>
+      <div className={styles['main-wrapper']}>
+        <h1>Calendar</h1>
+        <div className={styles['content-wrapper']}>
+          <div className={styles['calendar-container']}>
+            <ul className={styles['calendar-months']}>
+              {populateMonths()}
+            </ul>
+          </div>
+          <div className={styles['schedule-container']}>
+            <Schedule />
+          </div>
+        </div>
       </div>
     </main>
   )
